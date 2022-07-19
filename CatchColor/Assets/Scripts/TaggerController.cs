e37edf1b5a67231e1f6a48acfcd9c71307af1f0a
Kim So Yeon <s2023@e-mirim.hs.kr>
2022-07-20 00:42:33 +0900

    플레이어 기본 이동 완성, 아이템 구현 시도(#1)
    아이템 구현 보완 필요함.

---
+++ CatchColor/Assets/Scripts/TaggerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaggerController : PlayerController
+{
+    [SerializeField]
+    private int currentColor;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
