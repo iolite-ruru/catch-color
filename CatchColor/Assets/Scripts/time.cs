c055070359de8461e6fe7cc73e9adb201fc00243
kimjisu33 <indexkim33@gmail.com>
2022-08-24 13:48:23 +0900

    Revert "Merge pull request #46 from iolite-ruru/jisu"
    This reverts commit b7438279c1cb9d16d1408f5664354e863b0ece1d, reversing
    changes made to 5b65410f0fea8acd538724ee972abe785bf01bee.

--- CatchColor/Assets/Scripts/time.cs
+++ CatchColor/Assets/Scripts/time.cs
@@ -5,33 +5,13 @@ using Mirror;
 
 public class time : NetworkBehaviour
 {
-    [SyncVar(hook = nameof(SetLimitTime_Hook))]
-    public float LimitTime = 180;
-    public void SetLimitTime_Hook(float _, float time)
-    {
-        LimitTime = time;
-        if (Mathf.Round(LimitTime) > 60)
-            txt_Time.text = "남은 시간 : " + (int)(Mathf.Round(LimitTime)) / 60 + "분 " + (Mathf.Round(LimitTime)) % 60 + "초";
-        else txt_Time.text = "남은 시간 : " + Mathf.Round(LimitTime) + "초";
-    }
+    [SyncVar]
+    public float LimitTime=180;
     public Text txt_Time;
     public GameObject backImg;
     public GameObject endTxt;
-    [SyncVar(hook = nameof(SetWaitTime_Hook))]
+    [SyncVar]
     public float waitTime = 10;
-    public void SetWaitTime_Hook(float _, float time)
-    {
-
-        waitTime = time;
-        if (waitTime < 0)
-        {
-            if (isServer)
-            {
-                NetworkManager.singleton.StopHost();
-            }
-        }
-        NextTxt2.text = "자동으로  " + Mathf.Round(waitTime) + "초 뒤 화면 전환됩니다.";
-    }
     public GameObject NextTxt;
     public Text NextTxt2;
 
@@ -43,26 +23,34 @@ public class time : NetworkBehaviour
     }
     void Update()
     {
-        if (LimitTime > 0)
-        {
+        if (LimitTime > 1) { 
             LimitTime -= Time.deltaTime;
-
+            if (Mathf.Round(LimitTime) > 60)
+                txt_Time.text = "남은 시간 : " + (int)(Mathf.Round(LimitTime)) / 60 + "분 " + (Mathf.Round(LimitTime)) % 60 + "초";
+            else txt_Time.text = "남은 시간 : " + Mathf.Round(LimitTime) + "초";
         }
         else
         {
-
             txt_Time.text = "";
             backImg.SetActive(true);
             endTxt.SetActive(true);
             NextTxt.SetActive(true);
 
+            NextTxt2.text = "자동으로  " + Mathf.Round(waitTime) + "초 뒤 화면 전환됩니다.";
             waitTime -= Time.deltaTime;
-
-        }
+            if(waitTime <= 1)
+            {
+                Debug.Log("장면 전환!!!");
+                if (isServer)
+                {
+                    NetworkManager.singleton.StopHost();
+                }
+            }
+        }    
     }
 
+    
 
 
-
-
+    
 }
