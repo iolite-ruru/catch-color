33dad3d6740f4b89cbb92511ab6f68c689a1bcb7
kimjisu33 <indexkim33@gmail.com>
2022-08-24 13:47:47 +0900

    Revert "Merge pull request #50 from iolite-ruru/main"
    This reverts commit bd116254e7220162060e428a8cf57b18b5cedf74, reversing
    changes made to 58a17b24d70a912e134d519864cb967015e40ae0.

--- CatchColor/Assets/Scripts/character/InGameRunnerMover.cs
+++ CatchColor/Assets/Scripts/character/InGameRunnerMover.cs
@@ -8,9 +8,6 @@ public class InGameRunnerMover : CharacterMover
 {
     public static int deadCount = 0;
 
-    [SerializeField]
-    protected SkinnedMeshRenderer[] myMesh = new SkinnedMeshRenderer[2];
-
     [SyncVar(hook = nameof(SetPlayerState_Hook))]
     public State playerState;
     public void SetPlayerState_Hook(State _, State state)
@@ -20,11 +17,8 @@ public class InGameRunnerMover : CharacterMover
         if (hasAuthority)
         {
             isMovable = false;
-            cam = Camera.main;
-            cam.transform.SetParent(transform.Find("Head").transform);
-            cam.transform.localPosition = new Vector3(0f, 0.017f, -0.01f);
-            /*cam.transform.position = new Vector3(0f, 20f, -10f);
-            cam.transform.rotation = Quaternion.Euler(50f,0f,0f);*/
+            cam.transform.position = new Vector3(0f, 20f, -10f);
+            cam.transform.rotation = Quaternion.Euler(50f,0f,0f);
         }
         if(deadCount== FindObjectsOfType<InGameRunnerMover>().Length)
         {
@@ -37,16 +31,10 @@ public class InGameRunnerMover : CharacterMover
         } 
     }
 
-    public override void SetLayer(int layerIndex)
-    {
-        gameObject.layer = layerIndex;
-    }
-
-
     [ClientRpc]
     public void RpcRendererFalse()
     {
-        //renderer.enabled = false;
+        renderer.enabled = false;
     }
 
 
@@ -66,9 +54,6 @@ public class InGameRunnerMover : CharacterMover
             CmdSetPlayerCharacter(myRoomPlayer.playerColor); //나중에 닉네임 설정할때 수정해야함
 
             GameObject.Find("TextColor").GetComponent<Text>().text = myRoomPlayer.playerColor.ToString();
-
-            myMesh[0] = transform.Find("Head").GetComponent<SkinnedMeshRenderer>();
-            myMesh[1] = transform.Find("Body").GetComponent<SkinnedMeshRenderer>(); //오브젝트 계층 구조 변경 전
         }
     }
 
@@ -83,35 +68,4 @@ public class InGameRunnerMover : CharacterMover
         }
     }
 
-    [SyncVar(hook = nameof(SetPlayerColor_Hook))]
-    public MyColor playerColor;
-    public override void SetPlayerColor_Hook(MyColor oldColor, MyColor newColor)
-    {
-        if (renderer == null)
-        {
-            renderer = gameObject.GetComponent<Renderer>();
-        }
-        test(newColor);
-        //renderer.material.color = PlayerColor.GetColor(newColor); //술래면 고글 색 바꾸기
-    }
-    
-    public void test(MyColor newColor)
-    {
-        myMesh[0].material.color = PlayerColor.GetColor(newColor);
-        myMesh[1].material.color = PlayerColor.GetColor(newColor);
-    }
-
-    [Command]
-    public override void CmdSetColor(MyColor color, int idx)
-    {
-        playerColor = color;
-        SetLayer(idx);
-    }
-
-    //색상 변경
-    [Command]
-    protected override void CmdSetPlayerCharacter(MyColor color)
-    {
-        playerColor = color;
-    }
 }
