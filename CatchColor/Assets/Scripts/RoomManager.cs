9342d09405663506fd5d4da59ee1ac6f26d3cac5
Jisu Kim <81640695+kimjisu33@users.noreply.github.com>
2022-08-27 18:09:34 +0900

    ip로 호스트 열고 들어가기 (테스트 전)

--- CatchColor/Assets/Scripts/RoomManager.cs
+++ CatchColor/Assets/Scripts/RoomManager.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using UnityEngine.UI;
 
 public class RoomManager : NetworkRoomManager {
 
+    
+
     static int taggernum;
+
 
     public override void Start()
     {
